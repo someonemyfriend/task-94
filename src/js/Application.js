531a689e44a1b4f68af18a5027c9f10a015e22a0
Vasilia Staykova <someonemyfriend@gmail.com>
2023-02-11 02:28:06 +0200

    Add files via upload

--- src/js/Application.js
+++ src/js/Application.js
@@ -12,10 +12,33 @@ export default class Application extends EventEmitter {
     this.emojis = [];
     this.banana = "🍌";
     this.emit(Application.events.READY);
+    this.div = document.getElementById('emojis')
   }
   setEmojis(emojis) {
     this.emojis = emojis;
+    this.removeAllCildNodes(this.div)
+
+    this.emojis.forEach(element => {
+      let p = document.createElement('p')
+      p.textContent = element;
+      this.div.appendChild(p)
+      
+    });
   }
 
-  addBananas() {}
+
+  addBananas() {
+    // In the addBananas()method 
+    //you have to implement the map logic
+  const monkeys = this.emojis.map(element => {
+  return element += this.banana;
+  })
+  this.setEmojis(monkeys)
+
+  }
+  removeAllCildNodes(parent){
+    while(parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
 }
